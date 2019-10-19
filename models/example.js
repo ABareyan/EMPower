0d79e31f928050e8c9e36ca1f4bbad1c6d7e3d87
Arman Bareyan <abareyan@gmail.com>
2019-10-19 12:49:15 -0600

    Create database

--- models/example.js
+++ models/example.js
@@ -1,7 +1,50 @@
+// module.exports = function(sequelize, DataTypes) {
+//     var Example = sequelize.define("Example", {
+//         text: DataTypes.STRING,
+//         description: DataTypes.TEXT
+//     });
+//     return Example;
+// };
+
+
+
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
+    var Employee = sequelize.define("Employee", {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 160]
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            isEmail: true,
+            validate: {
+                len: [1, 160]
+            }
+        },
+        // eslint-disable-next-line camelcase
+        phone_number: {
+            type: DataTypes.INTEGER,
+            validate: {
+                len: [1, 50]
+            }
+        },
+        // eslint-disable-next-line camelcase
+        job_type: {
+            type: DataTypes.TEXT,
+            validate: {
+                len: [1, 160]
+            }
+        },
+        compensation: {
+            type: DataTypes.INTEGER,
+            validate: {
+                len: [1, 160]
+            }
+        }
+    });
+    return Employee;
 };
