c868880e03370c974f2c66e98b18f6207d46fd3e
Arman Bareyan <abareyan@gmail.com>
2019-10-21 23:00:00 -0600

    Update HTML

--- models/example.js
+++ models/example.js
@@ -6,45 +6,40 @@
 //     return Example;
 // };
 
-
-
-module.exports = function(sequelize, DataTypes) {
-    var Employee = sequelize.define("Employee", {
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1, 160]
-            }
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            isEmail: true,
-            validate: {
-                len: [1, 160]
-            }
-        },
-        // eslint-disable-next-line camelcase
-        phone_number: {
-            type: DataTypes.INTEGER,
-            validate: {
-                len: [1, 50]
-            }
-        },
-        // eslint-disable-next-line camelcase
-        job_type: {
-            type: DataTypes.TEXT,
-            validate: {
-                len: [1, 160]
-            }
-        },
-        compensation: {
-            type: DataTypes.INTEGER,
-            validate: {
-                len: [1, 160]
-            }
-        }
-    });
-    return Employee;
+module.exports = function (sequelize, DataTypes) {
+  var Employee = sequelize.define("Employee", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      isEmail: true
+    },
+    // eslint-disable-next-line camelcase
+    phone_number: {
+      type: DataTypes.INTEGER,
+      validate: {
+        len: [1, 50]
+      }
+    },
+    // eslint-disable-next-line camelcase
+    job_type: {
+      type: DataTypes.TEXT,
+      validate: {
+        len: [1, 160]
+      }
+    },
+    compensation: {
+      type: DataTypes.INTEGER,
+      validate: {
+        len: [1, 160]
+      }
+    }
+  });
+  return Employee;
 };
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,39 +1,49 @@
+var path = require("path");
+
 var db = require("../models");
 
-module.exports = function(app) {
-    app.get("/", function(req, res) {
-        res.render("index")
+module.exports = function (app) {
+  app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/main.html"));
+  });
+
+  app.get("/api/employee", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/employee.html"));
+  });
+
+  app.get("/api/company", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/html/company.html"));
+  });
+
+
+
+  // app.get("/api/employee", function (req, res) {
+  //   res.sendFile(path.join(__dirname, "../public/html/main.html"))
+  // });
+
+  // // Load index page
+  // app.get("/", function(req, res) {
+  //     db.Example.findAll({}).then(function(dbExamples) {
+  //         res.render("index", {
+  //             msg: "Welcome!",
+  //             examples: dbExamples
+  //         });
+  //     });
+  // });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function (req, res) {
+    db.Example.findOne({ where: { id: req.params.id } }).then(function (
+      dbExample
+    ) {
+      res.render("example", {
+        example: dbExample
+      });
     });
+  });
 
-
-
-
-
-
-
-    // // Load index page
-    // app.get("/", function(req, res) {
-    //     db.Example.findAll({}).then(function(dbExamples) {
-    //         res.render("index", {
-    //             msg: "Welcome!",
-    //             examples: dbExamples
-    //         });
-    //     });
-    // });
-
-    // Load example page and pass in an example by id
-    app.get("/example/:id", function(req, res) {
-        db.Example.findOne({ where: { id: req.params.id } }).then(function(
-            dbExample
-        ) {
-            res.render("example", {
-                example: dbExample
-            });
-        });
-    });
-
-    // Render 404 page for any unmatched routes
-    app.get("*", function(req, res) {
-        res.render("404");
-    });
+  // Render 404 page for any unmatched routes
+  app.get("*", function (req, res) {
+    res.render("404");
+  });
 };
